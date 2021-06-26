771d2545ec5303966f6fb1e0e313d9783c494c97
Akash Chauhan <developer@account.com>
2021-06-27 03:03:55 +0530

    Axios Instance working and globally used in vue Components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from './axiosInstance'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.config.globalProperties.$axios = axios;
+
+app.mount('#app')
